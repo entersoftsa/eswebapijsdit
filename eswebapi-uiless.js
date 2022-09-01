2bbc5806629a98a75fdd5785a1e78f70974ba41f
sme <sme@entersoft.gr>
2022-09-01 13:19:42 +0300

    3.1.0

--- eswebapi-uiless.js
+++ eswebapi-uiless.js
@@ -1,4 +1,4 @@
-/*! Entersoft Application Server WEB API - v3.0.14 - 2022-08-26
+/*! Entersoft Application Server WEB API - v3.1.0 - 2022-09-01
 * Copyright (c) 2022 Entersoft SA; Licensed Apache-2.0 */
 /***********************************
  * Entersoft SA
@@ -5875,7 +5875,7 @@ $scope.fetchES00DocumentsByEntityGID = function() {
         return window._; //Underscore must already be loaded on the page 
     });
 
-    var version = "3.0.14";
+    var version = "3.1.0";
     var vParts = _.map(version.split("."), function(x) {
         return parseInt(x);
     });
